9dada314d2ef4a65fbec589bcccd300c79aed299
jose-amat <jose.alc.luis@gmail.com>
2020-08-19 15:19:40 -0300

    add first user interface

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ This script will help you to extract the youtube comments from a video. We are c
 
 
 ```bash
-$ python youtube_api.py 
+$ python UI.py
 ```
+
+![](media/vscode_environment.png)
 
 ## Note
 - You have to pass a youtube key. To create it please see [here](https://developers.google.com/youtube/v3/docs).
 - You have to pass the URL with this format: https://www.youtube.com/watch?v=CWeffUwlbUA
+- Both tables will be saved in the Data folder.
---
+++ UI.py
@@ -0,0 +1,37 @@
+import youtube_api as yb
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title('Python Youtube comments')
+window['background'] = 'red'
+
+tk.Label(window, text="API Key").grid(row=0)
+tk.Label(window, text="Video URL").grid(row=1)
+
+key = tk.Entry(window)
+url = tk.Entry(window)
+
+
+def enter():
+  lb['text'] = 'Loading...'
+  yb.youtubeComments(key.get(), url.get())
+  lb['text'] = 'Finished.'
+  print('Finished.')
+
+key.grid(row=0, column=1)
+url.grid(row=1, column=1)
+
+quitBtn = tk.Button(window, width=5, text="Quit", command=window.quit).grid(row=3,
+                                                  column=1,
+                                                  sticky=tk.W,
+                                                  pady=2)
+
+bt = tk.Button(window, width=5, text="Enter", command=enter).grid(row=3,
+                                                  column=2,
+                                                  sticky=tk.W,
+                                                  pady=2)
+lb = tk.Label(window, text='Insert your data')
+lb.grid(row=4, column=1)
+
+window.mainloop()
