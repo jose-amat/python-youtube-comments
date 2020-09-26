077c7f094990695b309fbedb8cc5155fae253377
jose-amat <jose.alc.luis@gmail.com>
2020-09-26 09:18:22 -0300

    by Playlist UI

--- teste.py
+++
@@ -1,6 +0,0 @@
-import functions.youtube_api as yb
-
-key = 'AIzaSyDazwNBxt816APtEYPQl5jAGg0YTRwQyPM'
-playlist = 'PLlBw3F15IXehbaG16XZ0eokoNWDKjFzQ-'
-
-videos = yb.commentsByPlaylist(key, playlist)
